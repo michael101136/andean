009d21ba199207863b6b24419997235eba18f112
Michael Pimentel palomino <michaelspaziour@gmail.com>
2019-09-11 17:50:26 -0500

    asds
    asd

--- resources/views/assets/pagina/es/detalleTour.blade.php
+++ resources/views/assets/pagina/es/detalleTour.blade.php
@@ -1,7 +1,16 @@
 @extends('assets.pagina.es.layouts.master')
 
 @section('content')
+<style type="text/css">
+    .tap_acoordion{
+        text-align: justify;
+        border: 1px solid #d2d6e2;
+        background: #3a709e;
+        font-weight: normal;
+        color: #ffffff;
+    }
 
+</style>
 
  
          <section id="overview" class="header header-bg-9">
@@ -288,7 +297,54 @@
                                     <h2>Itinerario</h2>
                                  <div class="row">
                                      <div class="col-md-12">
-                                         
+                                            <div id="accordion">
+  <h3 class="tap_acoordion">Section 1</h3>
+  <div>
+    <p>
+    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+    </p>
+  </div>
+  <h3 class="tap_acoordion">Section 2</h3>
+  <div>
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+  <h3 class="tap_acoordion">Section 3</h3>
+  <div>
+    <p>
+    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+    </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+  <h3 class="tap_acoordion">Section 4</h3>
+  <div>
+    <p>
+    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+    mauris vel est.
+    </p>
+    <p>
+    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+    inceptos himenaeos.
+    </p>
+  </div>
+</div>
                                      </div>
 
                                     </div>
@@ -698,6 +754,12 @@
                     });
                 }
             });
+
 </script>
 
+<script>
+  $( function() {
+     $( "#accordion" ).accordion();
+ } );
+  </script>
 @endsection
