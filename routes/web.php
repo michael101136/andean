60eec037db001d52e3c932e920b70226c23b260a
Michael Pimentel palomino <michaelspaziour@gmail.com>
2019-09-11 23:08:17 -0500

    asd
    asd

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+Route::get('/', function () {
+    return redirect('/es');
+});
 
 Route:: get('/detalle_tour1','PublicController@detalleTour1')->name('detalle_tour1');
 
